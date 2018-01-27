1a16108890de4e43dc5162fa347b714c74124ccc
Roelof Roos <github@roelof.io>
2018-01-27 13:38:05 +0100

    Extensive logging for Polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,8 +7,16 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar topbar &
-polybar bottombar &
+(
+    polybar topbar 2>&1 3>&1 \
+    | ts '[%Y-%m-%d %H:%M:%S][top] ' \
+    >> $HOME/.config/polybar/log.log \
+&)
+(
+    polybar bottombar 2>&1 3>&1 \
+    | ts '[%Y-%m-%d %H:%M:%S][btn] ' \
+    >> $HOME/.config/polybar/log.log \
+&)
 
 
 echo "Bars launched..."
