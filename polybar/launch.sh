46fc6ecec920c3c97db358b2ecaea15389448c90
Roelof Roos <github@roelof.io>
2018-01-27 13:41:02 +0100

    Ensure github.token exists in polybar
    Otherwise the app will have issues.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,6 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Make sure GitHub key exists
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ ! -f "$DIR/github.token" ]; then
+    touch "$DIR/github.token"
+fi
+
 # Launch bar1 and bar2
 (
     polybar topbar 2>&1 3>&1 \
